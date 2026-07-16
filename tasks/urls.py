8f973a2aedd1a375058ca0e8e1cc79e8f6aa577a
vladiukdaria <279978409+vladiukdaria-debug@users.noreply.github.com>
2026-07-16 12:24:50 +0200

    impletent to do list

---
+++ tasks/urls.py
@@ -0,0 +1,54 @@
+from django.urls import path
+
+from tasks import views
+
+
+app_name = "tasks"
+
+urlpatterns = [
+    path(
+        "",
+        views.TaskListView.as_view(),
+        name="task-list",
+    ),
+    path(
+        "tasks/create/",
+        views.TaskCreateView.as_view(),
+        name="task-create",
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        views.TaskUpdateView.as_view(),
+        name="task-update",
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        views.TaskDeleteView.as_view(),
+        name="task-delete",
+    ),
+    path(
+        "tasks/<int:pk>/toggle/",
+        views.toggle_task_status,
+        name="task-toggle",
+    ),
+    path(
+        "tags/",
+        views.TagListView.as_view(),
+        name="tag-list",
+    ),
+    path(
+        "tags/create/",
+        views.TagCreateView.as_view(),
+        name="tag-create",
+    ),
+    path(
+        "tags/<int:pk>/update/",
+        views.TagUpdateView.as_view(),
+        name="tag-update",
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        views.TagDeleteView.as_view(),
+        name="tag-delete",
+    ),
+]
